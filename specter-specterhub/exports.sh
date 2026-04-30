ac91f89d6da870b566561a4c02baa37cd39f784c
Aleš Horvat <ales.horvat@gmail.com>
2026-04-30 23:56:56 +0200

    Sharing same SSH Password encryption key between apps

--- specter-specterhub/exports.sh
+++ specter-specterhub/exports.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 export APP_SPECTERHUB_DATA_DIR="${EXPORTS_APP_DIR}"
-_key=$(docker inspect specter-specterhub_specterhub_1 \
-  --format='{{range .Config.Env}}{{println .}}{{end}}' 2>/dev/null \
-  | grep '^SSH_PASSWORD_ENCRYPTION_KEY=' | cut -d= -f2- || true)
-[[ -n "${_key}" ]] && export APP_SPECTERHUB_SSH_KEY="${_key}"
+export APP_SPECTERHUB_SSH_KEY="$(derive_entropy "app-specter-specterhub-seed")"
